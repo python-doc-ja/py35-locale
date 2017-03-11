9e760b184b8ae583ee5033b398cf610496c8964e
cocoatomo <cocoatomo77@gmail.com>
2017-03-11 12:48:23 +0900

    Consider a case of no difference

--- update_github_pages.sh
+++ update_github_pages.sh
@@ -10,7 +10,10 @@ git pull origin master
 git --no-pager diff -G'最終更新日時: '
 git --no-pager diff --numstat | awk '$1 != 1 || $2 != 1 || $3 !~ /.html$/ { print $3 }'
 # gubed
-if [ $(git --no-pager diff -G'最終更新日時: ' | wc -l) -ge 1 ]; then
+if [ $(git status -s | wc -l) -eq 0 ]; then
+    echo "nothing to commit"
+    exit 0
+elif [ $(git --no-pager diff -G'最終更新日時: ' | wc -l) -ge 1 ]; then
     git --no-pager diff --numstat | awk '$1 != 1 || $2 != 1 || $3 !~ /.html$/ { print $3 }' | xargs git add
 else
     git add .
