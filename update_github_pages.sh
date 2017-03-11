4e591177ce7263e5e12d858161bee09b5f8e95b8
cocoatomo <cocoatomo77@gmail.com>
2017-03-11 12:09:23 +0900

    Gather commands into a script file

---
+++ update_github_pages.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -Ceux
+
+cd ../../py35
+git config user.email "travis-build-bot@example.com"
+git config user.name "Autobuild bot on Travis-CI"
+git config --local core.quotepath false
+git pull origin master
+if [ $(git --no-pager diff -G'最終更新日時: ' | wc -l) -ge 1 ]; then
+    git --no-pager diff --numstat | awk '$1 != 1 || $2 != 1 || $3 !~ /.html$/ { print $3 }' | xargs git add
+else
+    git add .
+fi
+git commit -a -m "update html"
+git push --quiet "https://${GH_TOKEN}@github.com/python-doc-ja/py35.git" master:master
