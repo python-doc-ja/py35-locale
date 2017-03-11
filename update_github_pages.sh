bb5c73115f2faaaaa63c40549fb212bdfdfcf0aa
cocoatomo <cocoatomo77@gmail.com>
2017-03-11 12:35:24 +0900

    Debug output

--- update_github_pages.sh
+++ update_github_pages.sh
@@ -6,6 +6,10 @@ git config user.email "travis-build-bot@example.com"
 git config user.name "Autobuild bot on Travis-CI"
 git config --local core.quotepath false
 git pull origin master
+# debug
+git --no-pager diff -G'最終更新日時: '
+git --no-pager diff --numstat | awk '$1 != 1 || $2 != 1 || $3 !~ /.html$/ { print $3 }'
+# gubed
 if [ $(git --no-pager diff -G'最終更新日時: ' | wc -l) -ge 1 ]; then
     git --no-pager diff --numstat | awk '$1 != 1 || $2 != 1 || $3 !~ /.html$/ { print $3 }' | xargs git add
 else
