4acf8cee53323548f22e6c390829ec3dafbba4c2
cocoatomo <cocoatomo77@gmail.com>
2017-03-12 09:45:55 +0900

    Refactor awk pattern expression

--- update_github_pages.sh
+++ update_github_pages.sh
@@ -7,13 +7,13 @@ git config --local core.quotepath false
 git pull origin master
 # debug
 git --no-pager diff -G'最終更新日時: '
-git --no-pager diff --numstat | awk '$1 != 1 || $2 != 1 || $3 !~ /.html$/ { print $3 }'
+git --no-pager diff --numstat | awk '! ($1 == 1 && $2 == 1 && $3 ~ /.html$/) { print $3 }'
 # gubed
 if [ $(git status -s | wc -l) -eq 0 ]; then
     echo "nothing to commit"
     exit 0
 elif [ $(git --no-pager diff -G'最終更新日時: ' | wc -l) -ge 1 ]; then
-    git --no-pager diff --numstat | awk '$1 != 1 || $2 != 1 || $3 !~ /.html$/ { print $3 }' | xargs git add
+    git --no-pager diff --numstat | awk '! ($1 == 1 && $2 == 1 && $3 ~ /.html$/) { print $3 }' | xargs git add
 else
     git add .
 fi
